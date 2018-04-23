0dbde388b674da002176035cc77ed4f3bac87f5b
Rex <rexebin@gmail.com>
2018-04-23 17:07:19 +0100

    fix: cache broke navigation, fixed.

--- src/commands/goto-parent.ts
+++ src/commands/goto-parent.ts
@@ -3,7 +3,8 @@ import {
   window,
   workspace,
   ViewColumn,
-  Range
+  Range,
+  Position
 } from "vscode";
 import { Config } from "../configuration";
 
@@ -12,17 +13,33 @@ export const gotoParentCommandName = "classLens.gotoParent";
  * Clicking on ClassLens Codelens will excute this command to open the target location.
  * @param symbol location to go to.
  */
+export interface SymbolRange {
+  line: number;
+  character: number;
+}
 export const gotoParent = (symbol: SymbolInformation) => {
+  const range: SymbolRange[] = <any>symbol.location.range;
   // The code you place here will be executed every time your command is executed
-  workspace.openTextDocument(symbol.location.uri).then(
+  workspace.openTextDocument(symbol.location.uri.fsPath).then(
     doc => {
-      window.showTextDocument(symbol.location.uri, {
-        viewColumn: Config.isSplit ? ViewColumn.Two : ViewColumn.Active,
-        selection: new Range(
-          symbol.location.range.start,
-          symbol.location.range.start
-        )
-      });
+      const startLine = range[0].line;
+      const startChar = range[0].character;
+      const endLine = range[1].line;
+      const endChar = range[1].character;
+      window
+        .showTextDocument(doc.uri, {
+          viewColumn: Config.isSplit ? ViewColumn.Two : ViewColumn.Active,
+          selection: new Range(
+            new Position(startLine, startChar),
+            new Position(endLine, endChar)
+          )
+        })
+        .then(
+          () => {},
+          error => {
+            console.log(error);
+          }
+        );
     },
     error => {
       throw error;
